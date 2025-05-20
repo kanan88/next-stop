3b7449561b87098242235fecfe97845896e99b32
Kanan Farzaliyev <kanan.farzali@gmail.com>
2025-05-20 09:36:34 +0200

    implement basic routing

---
+++ app/routes/admin/admin-layout.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from 'react-router'
+
+const AdminLayout = () => {
+  return (
+    <div className="admin-layout">
+      <aside className="children">
+        <Outlet />
+      </aside>
+    </div>
+  )
+}
+export default AdminLayout
