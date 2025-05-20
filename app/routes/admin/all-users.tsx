efe2c97f7cf974c572165abb4a5c913c8429627a
Kanan Farzaliyev <kanan.farzali@gmail.com>
2025-05-20 11:40:19 +0200

    implement primary layout with Header and Sidebars

--- app/routes/admin/all-users.tsx
+++ app/routes/admin/all-users.tsx
@@ -1,4 +1,14 @@
+import { Header } from 'components'
+
 const AllUsers = () => {
-  return <div>AllUsers</div>
+  return (
+    <main className="dashboard wrapper">
+      <Header
+        title="Trips Page"
+        description="Check out our current users in real time"
+      />
+      All Users Page Contents
+    </main>
+  )
 }
 export default AllUsers
