e21c683f759f84ce53f2c966adb55524ced3aafc
Kanan Farzaliyev <kanan.farzali@gmail.com>
2025-09-23 16:15:39 +0200

    add analytics

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react'
 import {
   isRouteErrorResponse,
   Links,
@@ -6,7 +7,6 @@ import {
   Scripts,
   ScrollRestoration
 } from 'react-router'
-
 import type { Route } from './+types/root'
 import './app.css'
 
@@ -46,7 +46,12 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
-  return <Outlet />
+  return (
+    <>
+      <Outlet />
+      <Analytics />
+    </>
+  )
 }
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
